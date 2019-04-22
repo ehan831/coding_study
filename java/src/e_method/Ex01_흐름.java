117d67b49df1622388492f41be84aba3ff3ba7ab
Eungu Han <ehan831@gmail.com>
2019-04-22 10:52:59 +0900

    no message

---
+++ java/src/e_method/Ex01_흐름.java
@@ -0,0 +1,13 @@
+package e_method;
+
+public class Ex01_흐름 {
+    public static void main(String[] args) {
+        System.out.println("메인 시작");
+        method();
+        System.out.println("메인 끝");
+    }
+
+    static void method() {
+        System.out.println("method 시작");
+    }
+}
